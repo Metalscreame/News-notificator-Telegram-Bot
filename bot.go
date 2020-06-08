41d0020205e08b525c965d3f3b5e0847b1ce9f9f
Roman <mantilocal@gmail.com>
2020-06-08 14:32:30 +0300

    fairy

--- bot.go
+++ bot.go
@@ -36,7 +36,7 @@ func (b *Bot) listenMessages() {
 
 		text, markup := b.AnnaMessageParser(message)
 		msg := tgbotapi.NewMessage(message.Chat.ID, text)
-		if markup != nil{
+		if markup != nil {
 			msg.ReplyMarkup = markup
 		}
 
@@ -74,18 +74,34 @@ func (b *Bot) MessageParser(msg *tgbotapi.Message) string {
 func (b *Bot) AnnaMessageParser(msg *tgbotapi.Message) (string, interface{}) {
 	switch msg.Text {
 	case Start:
-		button1 := tgbotapi.KeyboardButton{
-			Text: "Получить",
-		}
-		button2 := tgbotapi.KeyboardButton{
-			Text: "Отказаться",
-		}
+		button1 := tgbotapi.KeyboardButton{Text: "Начать"}
+		mrkup := tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{button1})
+		return `Я бот. Я был создан для того, чтобы поведать тебе сказку о принцессе Анне и об одном парне, который создал меня, по имени Р… впрочем ты и так уже знаешь кто он. Что ж… Начнем? Shall we?`, mrkup
+	case "Начать":
+		m := `В тридевятом царстве, в тридесятом государстве, жила-была принцесса по имени Аня. Она была необычайно красивой, доброй, отзывчивой и умной девушкой. Из-за разных перипетий в её жизни она немного грустила. Все мы так делаем время от времени. Но сегодня… Сегодня у неё был повод радоваться, ведь сегодня у неё – День Рождения! Светлый праздник у всего королевства в этот день. Все птички напевают её любимые мелодии на протяжении всего дня! И был у неё знакомый парень по имени...`
+		button1 := tgbotapi.KeyboardButton{Text: "Петр"}
+		button2 := tgbotapi.KeyboardButton{Text: "Рома"}
+		button3 := tgbotapi.KeyboardButton{Text: "Женя"}
+
+		mrkup := tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{button1, button2, button3})
+		return m, mrkup
+	case "Петр", "Женя":
+		return fmt.Sprintf("По имени %v. И он был парнем, который просто жил да был да как-то пересекался с принцессой Аней. Что на счет другого парня?", msg.Text), nil
+	case "Рома":
+		button1 := tgbotapi.KeyboardButton{Text: "Далее"}
+		m := `По имени Рома. Рома очень хотел поздравить Аню с днем рождения и очень хотел вызвать на её прекрасном лице улыбку радость в её сердце. И он очень хотел произвести на неё впечатление.`
+		mrkup := tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{button1})
+		return m, mrkup
+	case "Далее":
+		button1 := tgbotapi.KeyboardButton{Text: "Да"}
+		button2 := tgbotapi.KeyboardButton{Text: "Нет"}
+
+		m := "Принцесса сидела в своей башне и слушала пение птиц, как вдруг одна птичка залатела прям к ней в окно, села на подоконник рядом с ней и уставилась на неё. Это был ручной птенчик Ромы. Она его уже раньше встречала. Принцесса заметила, что к птенца ноге была привязана бумага. Раскрыть бумагу?"
 		mrkup := tgbotapi.NewReplyKeyboard([]tgbotapi.KeyboardButton{button1, button2})
-		return `Приветствую, принцесса :) Я всего лишь бот, но я был сделан для того, чтобы направить тебя на получение небольшого подарка, сделанного одним парнем, который хотел бы вызвать на твоем прекрасном лице улыбку  радость в твоем сердце. Напиши "Получить"" если хочешь получить подарок, либо /dl_Отказаться , если не желаешь его принять.`,
-			mrkup
-	case "Получить":
-		return `Хороший выбор! С Днем рождения! https://youtu.be/6H-InGqgKzo`, nil
-	case "Отказаться":
+		return m, mrkup
+	case "Да":
+		return `Хороший выбор! Вот то, что было написано на записке: "С Днем рождения, милая принцесса! https://youtu.be/6H-InGqgKzo"`, nil
+	case "Нет":
 		return `Ты уверенна, что хочешь разбить сердце этому парню? Я, конечно, всего лишь набор нулей и еденичек, но даже я чувствую как ты ему дорога. А я не обишаюсь, я ведь машина! Уж поверь. https://i.ytimg.com/vi/zUOxSYOzECU/maxresdefault.jpg`, nil
 	default:
 		return "Такого выбора нет...", nil
